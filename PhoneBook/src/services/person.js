6a75793031df566ea07beda97baa1e4042729b37
Babu-Mohammed-Izhan <69976015+Babu-Mohammed-Izhan@users.noreply.github.com>
2021-05-28 20:43:12 +0530

    Add files via upload

---
+++ PhoneBook/src/services/person.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/api/persons'
+
+const getAll = () => {
+    return axios.get(baseUrl)
+}
+
+const create = newObject => {
+    return axios.post(baseUrl, newObject)
+}
+
+const deleteNumber = id => {
+    return axios.delete(`http://localhost:3001/persons/${id}`)
+}
+const updateNumber = (id, updateObject) => {
+    console.log(id, updateObject);
+    return axios.put(`http://localhost:3001/persons/${id}`, updateObject)
+}
+
+export default { getAll, create, deleteNumber, updateNumber }
