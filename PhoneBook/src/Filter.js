7f2d7ca8020c2d4cd34ab3e5172af5d7f91cc6e3
Babu-Mohammed-Izhan <69976015+Babu-Mohammed-Izhan@users.noreply.github.com>
2021-05-28 20:39:15 +0530

    Delete PhoneBook/src directory

--- PhoneBook/src/Filter.js
+++
@@ -1,9 +0,0 @@
-function Filter({ handleSearch, newSearch }) {
-    return (
-        <div>
-            <h3>filter shown with <input value={newSearch} onChange={handleSearch} /></h3>
-        </div>
-    )
-}
-
-export default Filter
--- PhoneBook/src/Persons.js
+++
@@ -1,9 +0,0 @@
-const Persons = ({ persons }) => {
-    return (
-        <div>
-            {persons.map((props) => <p key={props.name}>{props.name} {props.number}</p>)}
-        </div>
-    )
-}
-
-export default Persons
